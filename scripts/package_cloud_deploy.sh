0ee3f9c95979600d95ed254d11001fa31e318e7c
Delemotte Barthélémy <barthelemy.delemotte@gmail.com>
2017-05-02 18:54:41 +0200

    Update travis CI (#94)
    * Dockerized build environment
    * Fix RPM and DEB names and versions
    * Deploy packages to packagecloud.io
    * Speed up travis compilation

---
+++ scripts/package_cloud_deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+MAGENTA='\033[1;35m'
+NOCOLOR='\033[0m'
+CYAN='\033[1;36m'
+NOCOLOR='\033[0m'
+
+echo_color() {
+    color="$1"
+    shift
+    text="$@"
+    echo -e "[${color}$text${NOCOLOR}]";
+}
+
+show_and_exec(){
+  echo_color $CYAN "$@"
+  eval "$@"
+}
+
+gem install package_cloud
+show_and_exec package_cloud push elassandra/latest/debian/jessie $DEBPKG
+show_and_exec package_cloud push elassandra/latest/el/7 $RPMPKG
