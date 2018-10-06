485844677faab477aa02fca7780f760915d6640a
Vincent Royer <vroyer@strapdata.com>
2018-10-06 10:55:14 +0200

    dockerfile refacto

--- docker/ready-probe.sh
+++ docker/ready-probe.sh
@@ -6,7 +6,7 @@
 
 if [[ $($CASSANDRA_HOME/bin/nodetool status 2>/dev/null | grep "$POD_IP" | awk '{ print $1 }') == "UN" ]]; then
   if [[ "$CASSANDRA_DAEMON" == "org.apache.cassandra.service.CassandraDaemon" ]] || \
-     [[ $(curl -XGET ${ELASSANDRA_MONITOR_PASSWORD:+"--user"} ${ELASSANDRA_MONITOR_PASSWORD:+"monitor:$ELASSANDRA_MONITOR_PASSWORD"} "${PROTOCOL:-http}://localhost:9200/_cat/nodes?h=ip" 2>/dev/null | grep "$POD_IP" ) == "$POD_IP" ]]; then
+     [[ $(curl -XGET "http://localhost:9200/_cat/nodes?h=ip" 2>/dev/null | grep "$POD_IP" ) == "$POD_IP" ]]; then
      exit 0;
   else 
      if [[ $DEBUG ]]; then
