43596ef18b11417aed6f002279bdade03a0cab16
vroyer <vroyer@strapdata.com>
2020-01-03 01:21:01 +0100

    Fix range field test due to date format with random Locale

--- server/src/test/java/org/elassandra/RangeFieldTests.java
+++ server/src/test/java/org/elassandra/RangeFieldTests.java
@@ -15,79 +15,30 @@
  */
 package org.elassandra;
 
-import com.carrotsearch.randomizedtesting.generators.RandomPicks;
-import com.google.common.net.InetAddresses;
-
-import org.apache.cassandra.config.DatabaseDescriptor;
-import org.apache.cassandra.cql3.UntypedResultSet;
-import org.apache.cassandra.cql3.UntypedResultSet.Row;
-import org.apache.cassandra.db.ConsistencyLevel;
-import org.apache.cassandra.db.marshal.DoubleType;
-import org.apache.cassandra.db.marshal.TupleType;
-import org.apache.cassandra.db.marshal.UTF8Type;
-import org.apache.cassandra.serializers.SimpleDateSerializer;
-import org.apache.cassandra.service.StorageService;
-import org.apache.cassandra.utils.UUIDGen;
 import org.apache.lucene.document.InetAddressPoint;
-import org.apache.lucene.index.DocValuesType;
-import org.apache.lucene.index.IndexableField;
-import org.apache.lucene.search.join.ScoreMode;
-import org.apache.lucene.util.LuceneTestCase.ThrowingRunnable;
-import org.elassandra.cluster.Serializer;
 import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
-import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.common.compress.CompressedXContent;
-import org.elasticsearch.common.geo.GeoPoint;
-import org.elasticsearch.common.geo.GeoUtils;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.mapper.DocumentMapper;
-import org.elasticsearch.index.mapper.MapperParsingException;
-import org.elasticsearch.index.mapper.ParsedDocument;
-import org.elasticsearch.index.mapper.RangeFieldMapper;
-import org.elasticsearch.index.mapper.SourceToParse;
-import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.test.ESSingleNodeTestCase;
-import org.elasticsearch.test.InternalSettingsPlugin;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.net.InetAddress;
-import java.nio.ByteBuffer;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.elasticsearch.index.query.RangeQueryBuilder.GTE_FIELD;
 import static org.elasticsearch.index.query.RangeQueryBuilder.GT_FIELD;
 import static org.elasticsearch.index.query.RangeQueryBuilder.LTE_FIELD;
 import static org.elasticsearch.index.query.RangeQueryBuilder.LT_FIELD;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
-import static org.hamcrest.Matchers.anyOf;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 /**
@@ -98,7 +49,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class RangeFieldTests extends ESSingleNodeTestCase {
 
     private static String FROM_DATE = "2016-10-31";
-    private static String TO_DATE = "2016-11-02 23:00:00";
+    private static String TO_DATE = "2016-11-05 23:00:00";
     private static String FROM_IP = "::ffff:c0a8:107";
     private static String TO_IP = "2001:db8::";
     private static int FROM = 5;
@@ -152,7 +103,7 @@ public class RangeFieldTests extends ESSingleNodeTestCase {
 
     private QueryBuilder getMiddleQuery(String type) throws ParseException {
         if (type.equals("date_range")) {
-            return QueryBuilders.termQuery("field", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2016-11-01 10:00:00").getTime());
+            return QueryBuilders.termQuery("field", new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse("2016-11-02").getTime());
         } else if (type.equals("long_range")) {
             return QueryBuilders.termQuery("field", 6L);
         } else if (type.equals("ip_range")) {
