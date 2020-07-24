0d9ae9e8ee34efd6233dec5013a7d387d8e081e4
vroyer <vroyer@strapdata.com>
2020-07-24 11:25:29 +0200

    release 6.2.3.29

--- docs/elassandra/source/conf.py
+++ docs/elassandra/source/conf.py
@@ -65,9 +65,9 @@ author = u'Strapdata'
 # built documents.
 #
 # The short X.Y version.
-version = u'6.2.3.28'
+version = u'6.2.3.29'
 # The full version, including alpha/beta/rc tags.
-release = u'6.2.3.28'
+release = u'6.2.3.29'
 
 tgz_url = "https://github.com/strapdata/elassandra/releases/download/v{0}/elassandra-{0}.tar.gz".format(release)
 deb_url = "https://github.com/strapdata/elassandra/releases/download/v{0}/elassandra-{0}.deb".format(release)
